e9c6b19d04bda72d94c12b80d0637a57a8e71602
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-12-26 14:52:03 +0900

    Simplify normal calculation and lighting in PSModel.hlsl
    Removed normalization of the normal vector and refactored lighting calculation to directly multiply albedo by light color, dot product, and light factor. This streamlines the shader code and may improve performance.

--- Asset/Shader/PSModel.hlsl
+++ Asset/Shader/PSModel.hlsl
@@ -35,11 +35,9 @@ float4 main(PixelInput input) : SV_TARGET
     float4 albedo = albedoTexture.Sample(textureSampler, input.UV) * albedoFactor;
     float3 normalMap = normalTexture.Sample(textureSampler, input.UV).xyz * 2.0f - 1.0f;
     
-    float3 normal = normalize(mul(normalMap, input.TBN)); // 이것도 normalize 꼭 필요하나?
+    float3 normal = mul(normalMap, input.TBN);
     float3 lightDir = lightDirection.xyz;
-    float NdotL = dot(normal, lightDir);
-    
-    albedo.rgb *= saturate(NdotL) * lightColor.rgb * lightFactor;
+    albedo.rgb *= lightColor.rgb * dot(normal, lightDir) * lightFactor;
     
     return albedo;
 }
