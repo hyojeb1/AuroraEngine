14c2e06078240c0b99a5c585913199272d238fd9
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-01-08 15:53:53 +0900

    Update VSCoordinateLine.hlsl

--- Asset/Shader/VSCoordinateLine.hlsl
+++ Asset/Shader/VSCoordinateLine.hlsl
@@ -10,19 +10,9 @@ VS_OUTPUT main(uint vertexID : SV_VertexID, uint instanceID : SV_InstanceID)
 {
     VS_OUTPUT output;
     
-    
-    if (instanceID < 101) // X방향 선
-    {
-        output.WorldPosition = float4(vertexID * 100.0f - 50.0f, 0.0f, float(instanceID) - 50.0f, 1.0f);
-    }
-    else if (instanceID < 202) // Z방향 선
-    {
-        output.WorldPosition = float4(float(instanceID) - 151.0f, 0.0f, vertexID * 100.0f - 50.0f, 1.0f);
-    }
-    else // Y축 선
-    {
-        output.WorldPosition = float4(0.0f, vertexID * 100.0f - 50.0f, 0.0f, 1.0f);
-    }
+    if (instanceID < 101) output.WorldPosition = float4(vertexID * 100.0f - 50.0f, 0.0f, float(instanceID) - 50.0f, 1.0f);
+    else if (instanceID < 202) output.WorldPosition = float4(float(instanceID) - 151.0f, 0.0f, vertexID * 100.0f - 50.0f, 1.0f);
+    else output.WorldPosition = float4(0.0f, vertexID * 100.0f - 50.0f, 0.0f, 1.0f);
     
     output.Position = mul(output.WorldPosition, VPMatrix);
     output.WorldPosition.xz = abs(output.WorldPosition.xz * 0.05f);
