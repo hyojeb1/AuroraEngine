8bcae30e30e7cd232a9f2b29b8f379ae66fd65cf
BIRANG22 <yundongmin9@gmail.com>
2026-01-16 16:10:17 +0900

    네트워크 제거

--- Client/main.cpp
+++ Client/main.cpp
@@ -5,7 +5,6 @@
 
 #include "TestScene.h"
 #include "HyojeTestScene.h"
-#include "ClientTestScene.h"
 
 #ifdef _DEBUG
 int main()
@@ -26,7 +25,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	windowManager.Initialize(L"Aurora");
 
 	SceneManager& sceneManager = SceneManager::GetInstance();
-	sceneManager.ChangeScene("ClientTestScene");
+	sceneManager.ChangeScene("HyojeTestScene");
 
 	while (windowManager.ProcessMessages()) sceneManager.Run();
 
