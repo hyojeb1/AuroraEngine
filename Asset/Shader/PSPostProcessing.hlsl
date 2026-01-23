19e78f1802bd4c65efdf3f03e033673d0be3324c
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-01-23 19:54:16 +0900

    Revert "Add gamma correction to post-processing pipeline"
    This reverts commit bb843a8813343dbbb384c76c6a920decb5462f09.

--- Asset/Shader/PSPostProcessing.hlsl
+++ Asset/Shader/PSPostProcessing.hlsl
@@ -4,9 +4,6 @@ float4 main(PS_INPUT_POS_UV input) : SV_TARGET
 {
     float4 color = sceneTexture.Sample(SamplerPointClamp, input.UV);
     
-    // 감마 보정
-    color.rgb = pow(color.rgb, Gamma);
-    
     if (PostProcessingFlags & 0x1) // 그레이스케일
     {
         float gray = dot(color.rgb, float3(0.299f, 0.587f, 0.114f));
