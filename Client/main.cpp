28138ebaac9762054e502f7ceaf526c1869c1d98
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-01-27 14:18:48 +0900

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -27,7 +27,7 @@ int main()
 	windowManager.Initialize(L"Aurora");
 
 	SceneManager& sceneManager = SceneManager::GetInstance();
-	sceneManager.ChangeScene("TaehyeonTestScene");
+	sceneManager.ChangeScene("TestScene");
 
 	SoundManager& soundManager = SoundManager::GetInstance();
 	soundManager.Initialize();
