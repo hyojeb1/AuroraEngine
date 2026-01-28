ac8470677514173c1ea084abfc37cfbf279f4f5d
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-01-28 16:10:37 +0900

    Update VSParticle.hlsl

--- Asset/Shader/VSParticle.hlsl
+++ Asset/Shader/VSParticle.hlsl
@@ -14,9 +14,7 @@ VS_OUTPUT_POS_UV main(VS_INPUT_POS_UV input, uint instanceID : SV_InstanceID)
 
     float4 worldPos = float4((centerWorldPos + positionOffset), 1.0f);
     
-    worldPos.xyz += rand(instanceID) * rand3(instanceID, SpreadRadius) * SpreadDistance * EclipsedTime;
-    
-    worldPos.xyz = mul(worldPos.xyz, (float3x3)WorldMatrix);
+    worldPos.xyz += mul(rand(instanceID) * rand3(instanceID, SpreadRadius) * SpreadDistance * EclipsedTime, (float3x3)WorldMatrix);
     
     output.Position = mul(worldPos, VPMatrix);
     output.UV = (input.UV * UVScale) + UVOffset;
