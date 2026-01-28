7befa8e4ebcb361652fd5d896e424caac12fb1b1
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-01-28 16:29:19 +0900

    Remove space in type cast for WorldMatrix multiplication
    Updated HLSL shaders to remove the space between (float3x3) and WorldMatrix in matrix multiplication expressions for consistency and style.

--- Asset/Shader/VSParticle_Cylindrical.hlsl
+++ Asset/Shader/VSParticle_Cylindrical.hlsl
@@ -13,7 +13,7 @@ VS_OUTPUT_POS_UV main(VS_INPUT_POS_UV input, uint instanceID : SV_InstanceID)
     float3 positionOffset = (flatRight * input.Position.x + float3(0.0f, 1.0f, 0.0f) * input.Position.y) * ImageScale;
     
     float4 worldPos = float4((centerWorldPos + positionOffset), 1.0f);
-    worldPos.xyz += mul(rand(instanceID) * rand3(instanceID, SpreadRadius) * SpreadDistance * EclipsedTime, (float3x3) WorldMatrix);
+    worldPos.xyz += mul(rand(instanceID) * rand3(instanceID, SpreadRadius) * SpreadDistance * EclipsedTime, (float3x3)WorldMatrix);
     
     output.Position = mul(worldPos, VPMatrix);
     output.UV = (input.UV * UVScale) + UVOffset;
